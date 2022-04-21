1a7c28962089c8bc8fff8705db41109d2c482e39
Guido Perdomo <guidop91@gmail.com>
2022-04-21 15:55:28 -0400

    po na valol

--- pages/index.js
+++ pages/index.js
@@ -1,96 +1,20 @@
-import Head from 'next/head';
-import MainFooter from '../components/main-footer';
-import MainHeader from '../components/main-header';
-import HomeCard from '../components/home-card';
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
-
-      <main>
-        <MainHeader />
-
-        <div className="grid">
-          <HomeCard
-            href="https://nextjs.org/docs"
-            heading="Documentation"
-            description="Find in-depth information about Next.js features and API."
-          />
-          <HomeCard
-            href="https://nextjs.org/learn"
-            heading="Learn"
-            description="Learn about Next.js in an interactive course with quizzes!"
-          />
-          <HomeCard
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            heading="Examples"
-            description="Discover and deploy boilerplate example Next.js projects."
-          />
-          <HomeCard
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            heading="Deploy"
-            description="Instantly deploy your Next.js site to a public URL with Vercel."
-          />
-        </div>
-      </main>
-
-      <MainFooter />
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  );
+      <section className={utilStyles.headingMd}>
+        <p>Well this is much cooler than I thought.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
 }
